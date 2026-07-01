fd67dc08b4bcc0fe3ba2a90fe190861808182442
MagarAZay <magarazay6@gmail.com>
2026-07-01 20:36:25 +0545

    git push

---
+++ 04-comments.js
@@ -0,0 +1,8 @@
+//This is single line comment
+//Anything right side of the // is considered as comment
+
+
+/*This is multi line comment
+This is second line of multi line comment
+This is third line of multi line comment
+The comment ends here*/
